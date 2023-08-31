ac89a02097b5890b02da91d42f83c3a0cbf80d66
BrunoMarinhoMarques <brunohenriw1@gmail.com>
2023-08-31 06:42:30 -0300

    Variaveis e Constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //VARIAVEIS E CONSTANTES
+
+        //variaveis
+        int numero = 5;
+        numero = 10;
+        System.out.print(numero);
+
+        double salarioMinimo = 2500.55;
+        short numeroCurto = 1;
+        String meuNome = "BRUNO HENRIQUE MARINHO MARQUES";
+        //constantes(valor que nao pode ser modificado depois de declarado)
+        final double VALOR_DE_PI = 3.14;
+
+        
+
+
     }
 }
