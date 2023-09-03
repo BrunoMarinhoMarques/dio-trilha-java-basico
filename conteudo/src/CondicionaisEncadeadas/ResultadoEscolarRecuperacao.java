a698af62f3ff68a36f335ed5fc9cb5de34e5ca52
BrunoMarinhoMarques <brunohenriw1@gmail.com>
2023-09-03 14:28:23 -0300

    Aprendendo um pouco sobre Condicional Encadeada

---
+++ conteudo/src/CondicionaisEncadeadas/ResultadoEscolarRecuperacao.java
@@ -0,0 +1,10 @@
+package CondicionaisEncadeadas;
+
+public class ResultadoEscolarRecuperacao {
+    public static void main(String[] args) {
+        int nota = 2;
+        if(nota>=7)System.out.println("Aprovado");
+        else if(nota>5&&nota<7)System.out.println("Recuperação");
+        else System.out.println("Reprovado");
+    }
+}
