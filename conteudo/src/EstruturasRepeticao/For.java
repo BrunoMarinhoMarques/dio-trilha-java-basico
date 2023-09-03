682a07b184358ca3fc07fc0d12d5f4acc42fc63e
BrunoMarinhoMarques <brunohenriw1@gmail.com>
2023-09-03 14:55:57 -0300

    Aprendendo um pouco sobre For

---
+++ conteudo/src/EstruturasRepeticao/For.java
@@ -0,0 +1,12 @@
+package EstruturasRepeticao;
+
+public class For {
+    public static void main(String [] args){
+
+        for(int carneirinhos=1; carneirinhos<=20;carneirinhos++){
+
+            System.out.println("Carneirinho = "+carneirinhos);
+
+        }
+    }
+}
