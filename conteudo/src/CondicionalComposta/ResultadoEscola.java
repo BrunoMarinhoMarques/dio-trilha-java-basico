e26b9e69cedadf7f40467b1c22f3aa53140f0b45
BrunoMarinhoMarques <brunohenriw1@gmail.com>
2023-09-03 14:20:24 -0300

    Aprendendo um pouco sobre Condicional Composta

---
+++ conteudo/src/CondicionalComposta/ResultadoEscola.java
@@ -0,0 +1,13 @@
+package CondicionalComposta;
+
+public class ResultadoEscola {
+    public static void main(String[] args) {
+        
+        int nota = 6;
+
+        if(nota>7)System.out.println("APROVADO");
+        else System.out.println("REPROVADO");
+
+
+    }
+}
