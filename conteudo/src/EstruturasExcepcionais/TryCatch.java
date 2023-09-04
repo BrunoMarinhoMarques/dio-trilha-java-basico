3d42b5f14bb2c79e4a8f2ded6757fbcb7c265e25
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-09-04 13:37:37 -0300

    Aprendendo um pouco sobre Try e Catch

---
+++ conteudo/src/EstruturasExcepcionais/TryCatch.java
@@ -0,0 +1,35 @@
+package EstruturasExcepcionais;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Locale;
+public class TryCatch {
+    public static void main(String[] args) {
+       
+       try{
+        String nome;
+        int idade;
+        double altura;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        nome = scan.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        idade = scan.nextInt();
+
+       
+
+        System.out.println("Digite sua altura: ");
+        altura = scan.nextDouble();
+
+    
+
+        System.out.println("Nome: "+nome+"\nIdade: "+idade+"\nAltura: "+altura);
+        
+    }catch(InputMismatchException e){
+        System.out.println("Os campos altura e idade precisam ser numéricos!");
+    }
+  }
+
+}
